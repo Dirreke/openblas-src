41fdcc4b452a7e410c8613312212dbca2410c2b6
Dirreke <mingyang_ge@163.com>
2025-08-19 22:57:17 +0800

    add some logs

--- openblas-src/build.rs
+++ openblas-src/build.rs
@@ -180,7 +180,10 @@ fn build() {
     } else {
         PathBuf::from(env::var("OUT_DIR").unwrap())
     };
-    let source = openblas_build::download(&output).unwrap();
+    let source = openblas_build::download(&output);
+    if let Err(e) = source {
+        panic!("Failed to download OpenBLAS source: {}", e);
+    }
 
     // If OpenBLAS is build as shared, user of openblas-src will have to find `libopenblas.so` at runtime.
     //
